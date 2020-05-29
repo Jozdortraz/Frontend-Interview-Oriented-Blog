343dfb97ed888ff7e6da3c4284e8d60a16df3b8a
Jozdortraz <jozdortraz@gmail.com>
2020-05-29 15:28:25 +0800

    feat: temporarily remove Angular version

--- lacerta-blog/copy404.js
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-const path = require('path');
-
-fs.copyFileSync(path.resolve(__dirname, './src/404.html'), path.resolve(__dirname, '../docs/404.html'));
--- lacerta-blog/src/utils/url-segment-object.ts
+++
@@ -1,8 +0,0 @@
-export const getURLSegmentObject = (path: string): string[] => {
-  try {
-    return path.split('/').filter(s => s.length > 0);
-  } catch (e) {
-    console.error(e);
-  }
-  return [];
-};
